f582f0881fffbd85b59ae43a900e48d2efae1bb3
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-06-13 16:26:34 +0300

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useTheme } from "../hooks/useTheme";
 import "./Navber.css";
 import Searcbar from "./Searchbar";
-import { ThemeContext } from "../context/ThemeContext";
 
 export default function Navbar() {
-    const { color } = useContext(ThemeContext);
+    const { color, changeColor } = useTheme();
 
     return (
         <div className="navbar" style={{ background: color }}>
-            <nav>
+            <nav onChange={() => changeColor('pink')}>
                 <Link to="/" className="brand">
                     <h1>Coooking Ninja</h1>
                 </Link>
